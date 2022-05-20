ef42dfd54f2160c3b74b479a004e00a13d0b1b96
DerToaster98 <seiler.oli@web.de>
2022-05-20 21:29:54 +0200

    use the crossbow's advantage pillagers!

---
+++ src/main/java/de/dertoaster/vanillaRevamps/mixin/RangedCrossbowAttackGoalMixin.java
@@ -0,0 +1,68 @@
+package de.dertoaster.vanillaRevamps.mixin;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.goal.RangedCrossbowAttackGoal;
+import net.minecraft.world.entity.ai.goal.RangedCrossbowAttackGoal.CrossbowState;
+import net.minecraft.world.entity.monster.CrossbowAttackMob;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.monster.RangedAttackMob;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.item.CrossbowItem;
+import net.minecraft.world.item.ItemStack;
+
+@Mixin(RangedCrossbowAttackGoal.class)
+public abstract class RangedCrossbowAttackGoalMixin<T extends Monster & RangedAttackMob & CrossbowAttackMob> extends Goal{
+	
+	@Shadow @Final T mob;
+	@Shadow int attackDelay;
+	@Shadow RangedCrossbowAttackGoal.CrossbowState crossbowState;
+	
+	@Inject(
+			at = @At("TAIL"),
+			method = "isValidTarget()Z",
+			cancellable = true
+			)
+	private void mixinIsValidTarget(CallbackInfoReturnable<Boolean> cir) {
+		if(!cir.getReturnValue()) {
+			RangedCrossbowAttackGoal<?> self = (RangedCrossbowAttackGoal<?>)(Object)this;
+			cir.setReturnValue(self.crossbowState == CrossbowState.CHARGING || self.crossbowState == CrossbowState.UNCHARGED);
+		}
+	}
+	
+	@Inject(
+			at = @At("HEAD"),
+			method = "tick()V",
+			cancellable = true
+			)
+	private void mixinTick(CallbackInfo ci) {
+		if(this.mob != null && this.mob.getTarget() == null) {
+			if (this.crossbowState == RangedCrossbowAttackGoal.CrossbowState.UNCHARGED) {
+	               this.mob.startUsingItem(ProjectileUtil.getWeaponHoldingHand(this.mob, item -> item instanceof CrossbowItem));
+	               this.crossbowState = RangedCrossbowAttackGoal.CrossbowState.CHARGING;
+	               this.mob.setChargingCrossbow(true);
+	         } else if (this.crossbowState == RangedCrossbowAttackGoal.CrossbowState.CHARGING) {
+	            if (!this.mob.isUsingItem()) {
+	               this.crossbowState = RangedCrossbowAttackGoal.CrossbowState.UNCHARGED;
+	            }
+
+	            int i = this.mob.getTicksUsingItem();
+	            ItemStack itemstack = this.mob.getUseItem();
+	            if (i >= CrossbowItem.getChargeDuration(itemstack)) {
+	               this.mob.releaseUsingItem();
+	               this.crossbowState = RangedCrossbowAttackGoal.CrossbowState.CHARGED;
+	               this.attackDelay = 20 + this.mob.getRandom().nextInt(20);
+	               this.mob.setChargingCrossbow(false);
+	            }
+	         }
+		}
+	}
+	
+}
