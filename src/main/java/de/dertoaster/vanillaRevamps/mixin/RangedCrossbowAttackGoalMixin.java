f974b97b74af1a91a9f887e9c81e9a0208ce7e7a
DerToaster98 <seiler.oli@web.de>
2022-05-20 21:34:17 +0200

    add comment and cancel nodes

--- src/main/java/de/dertoaster/vanillaRevamps/mixin/RangedCrossbowAttackGoalMixin.java
+++ src/main/java/de/dertoaster/vanillaRevamps/mixin/RangedCrossbowAttackGoalMixin.java
@@ -19,50 +19,51 @@ import net.minecraft.world.item.CrossbowItem;
 import net.minecraft.world.item.ItemStack;
 
 @Mixin(RangedCrossbowAttackGoal.class)
-public abstract class RangedCrossbowAttackGoalMixin<T extends Monster & RangedAttackMob & CrossbowAttackMob> extends Goal{
-	
-	@Shadow @Final T mob;
-	@Shadow int attackDelay;
-	@Shadow RangedCrossbowAttackGoal.CrossbowState crossbowState;
-	
-	@Inject(
-			at = @At("TAIL"),
-			method = "isValidTarget()Z",
-			cancellable = true
-			)
+public abstract class RangedCrossbowAttackGoalMixin<T extends Monster & RangedAttackMob & CrossbowAttackMob> extends Goal {
+
+	@Shadow
+	@Final
+	T mob;
+	@Shadow
+	int attackDelay;
+	@Shadow
+	RangedCrossbowAttackGoal.CrossbowState crossbowState;
+
+	@Inject(at = @At("TAIL"), method = "isValidTarget()Z", cancellable = true)
 	private void mixinIsValidTarget(CallbackInfoReturnable<Boolean> cir) {
-		if(!cir.getReturnValue()) {
-			RangedCrossbowAttackGoal<?> self = (RangedCrossbowAttackGoal<?>)(Object)this;
+		if (!cir.getReturnValue()) {
+			RangedCrossbowAttackGoal<?> self = (RangedCrossbowAttackGoal<?>) (Object) this;
 			cir.setReturnValue(self.crossbowState == CrossbowState.CHARGING || self.crossbowState == CrossbowState.UNCHARGED);
 		}
 	}
-	
-	@Inject(
-			at = @At("HEAD"),
-			method = "tick()V",
-			cancellable = true
-			)
-	private void mixinTick(CallbackInfo ci) {
-		if(this.mob != null && this.mob.getTarget() == null) {
-			if (this.crossbowState == RangedCrossbowAttackGoal.CrossbowState.UNCHARGED) {
-	               this.mob.startUsingItem(ProjectileUtil.getWeaponHoldingHand(this.mob, item -> item instanceof CrossbowItem));
-	               this.crossbowState = RangedCrossbowAttackGoal.CrossbowState.CHARGING;
-	               this.mob.setChargingCrossbow(true);
-	         } else if (this.crossbowState == RangedCrossbowAttackGoal.CrossbowState.CHARGING) {
-	            if (!this.mob.isUsingItem()) {
-	               this.crossbowState = RangedCrossbowAttackGoal.CrossbowState.UNCHARGED;
-	            }
 
-	            int i = this.mob.getTicksUsingItem();
-	            ItemStack itemstack = this.mob.getUseItem();
-	            if (i >= CrossbowItem.getChargeDuration(itemstack)) {
-	               this.mob.releaseUsingItem();
-	               this.crossbowState = RangedCrossbowAttackGoal.CrossbowState.CHARGED;
-	               this.attackDelay = 20 + this.mob.getRandom().nextInt(20);
-	               this.mob.setChargingCrossbow(false);
-	            }
-	         }
+	@Inject(at = @At("HEAD"), method = "tick()V", cancellable = true)
+	private void mixinTick(CallbackInfo ci) {
+		// Necessary to not conflict with vanilla, vanilla does this at the first line
+		if (this.mob != null && this.mob.getTarget() == null) {
+			if (this.crossbowState == RangedCrossbowAttackGoal.CrossbowState.UNCHARGED) {
+				this.mob.startUsingItem(ProjectileUtil.getWeaponHoldingHand(this.mob, item -> item instanceof CrossbowItem));
+				this.crossbowState = RangedCrossbowAttackGoal.CrossbowState.CHARGING;
+				this.mob.setChargingCrossbow(true);
+
+				ci.cancel();
+			} else if (this.crossbowState == RangedCrossbowAttackGoal.CrossbowState.CHARGING) {
+				if (!this.mob.isUsingItem()) {
+					this.crossbowState = RangedCrossbowAttackGoal.CrossbowState.UNCHARGED;
+				}
+
+				int i = this.mob.getTicksUsingItem();
+				ItemStack itemstack = this.mob.getUseItem();
+				if (i >= CrossbowItem.getChargeDuration(itemstack)) {
+					this.mob.releaseUsingItem();
+					this.crossbowState = RangedCrossbowAttackGoal.CrossbowState.CHARGED;
+					this.attackDelay = 20 + this.mob.getRandom().nextInt(20);
+					this.mob.setChargingCrossbow(false);
+				}
+				
+				ci.cancel();
+			}
 		}
 	}
-	
+
 }
