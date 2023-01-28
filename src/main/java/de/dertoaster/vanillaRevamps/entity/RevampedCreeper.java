8b4706f891bba15719ebd94986bda477255aee46
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-01-28 12:02:30 +0100

    drop armor on explosion

--- src/main/java/de/dertoaster/vanillaRevamps/entity/RevampedCreeper.java
+++ src/main/java/de/dertoaster/vanillaRevamps/entity/RevampedCreeper.java
@@ -7,16 +7,20 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.EquipmentSlot.Type;
 import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.phys.Vec3;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.IAnimationTickable;
 import software.bernie.geckolib3.core.PlayState;
@@ -99,6 +103,11 @@ public class RevampedCreeper extends Creeper implements IAnimatable, IAnimationT
 	public SpawnGroupData finalizeSpawn(ServerLevelAccessor pLevel, DifficultyInstance pDifficulty, MobSpawnType pReason, SpawnGroupData pSpawnData, CompoundTag pDataTag) {
 		this.populateDefaultEquipmentSlots(pLevel.getRandom(), pDifficulty);
 		this.populateDefaultEquipmentEnchantments(pLevel.getRandom(), pDifficulty);
+		
+		//this.setItemSlot(EquipmentSlot.HEAD, Items.NETHERITE_HELMET.getDefaultInstance());
+		//this.setItemSlot(EquipmentSlot.CHEST, Items.NETHERITE_CHESTPLATE.getDefaultInstance());
+		//this.setItemSlot(EquipmentSlot.LEGS, Items.NETHERITE_LEGGINGS.getDefaultInstance());
+		//this.setItemSlot(EquipmentSlot.FEET, Items.NETHERITE_BOOTS.getDefaultInstance());
 
 		return super.finalizeSpawn(pLevel, pDifficulty, pReason, pSpawnData, pDataTag);
 	}
@@ -106,7 +115,44 @@ public class RevampedCreeper extends Creeper implements IAnimatable, IAnimationT
 	public float getSwell() {
 		return this.swell;
 	}
-
+	
+	@Override
+	protected void explodeCreeper() {
+		if(!this.level.isClientSide()) {
+			for(EquipmentSlot slot : EquipmentSlot.values()) {
+				if(slot.getType() == Type.ARMOR) {
+					ItemStack stack = this.getItemBySlot(slot);
+					
+					ItemEntity itemEnt = this.spawnAtLocation(stack, this.getBbHeight() * 0.66F);
+					if(itemEnt != null) {
+						this.setItemSlot(slot, ItemStack.EMPTY);
+						
+						float vx = -0.5F * this.level.getRandom().nextFloat();
+						vx *= 1.0F + 0.5F * this.level.getRandom().nextFloat();
+						
+						float vz = -0.5F * this.level.getRandom().nextFloat();
+						vz *= 1.0F + 0.5F * this.level.getRandom().nextFloat();
+						
+						float vy = 0.125F + 0.25F* this.level.getRandom().nextFloat();
+						
+						Vec3 velocity = new Vec3(vx, vy, vz);
+						velocity.scale((double)(this.getBbWidth() * 0.5F));
+						itemEnt.setPos(itemEnt.position().add(velocity));
+						
+						itemEnt.setDeltaMovement(vx, vy, vz);
+						itemEnt.setInvulnerable(true);
+					}
+				}
+			}
+		}
+		super.explodeCreeper();
+	}
+	
+	@Override
+	protected void dropEquipment() {
+	}
+	
+	@SuppressWarnings("deprecation")
 	@Override
 	public ItemStack getPickResult() {
 		SpawnEggItem spawneggitem = SpawnEggItem.byId(EntityType.CREEPER);
